17391421927576a62c82e7db478d6c8ab4d5828d
Brandon Wolvin <wolvinb1@uoiitwolvinb1.local>
2018-05-19 00:23:46 -0400

    clean up

--- src/components/users/userList.js
+++ src/components/users/userList.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import UserListItem from './userListItem';
 
 class UserList extends Component {
-	render() {
-		return (
-			<div className="user-list">
-				<ul>
-					{this.props.users.map(user => (
-						<UserListItem user={user} key={`${user.name.first}-${user.name.last}`} />
-					))}
-				</ul>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className="user-list">
+                <ul>
+                    {this.props.users.map(user => (
+                        <UserListItem user={user} key={`${user.name.first}-${user.name.last}`} />
+                    ))}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default UserList;
