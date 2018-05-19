15890e8dfc60b374614096f1367a9bc24508421e
Brandon Wolvin <wolvinb1@uoiitwolvinb1.local>
2018-05-18 23:43:33 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -6,74 +6,74 @@ import { getInitialUserListData } from './api/requestApi';
 import Spinner from 'reactjs-simple-spinner';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      users: [],
-      filteredUsers: [],
-      loading: false
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			users: [],
+			filteredUsers: [],
+			loading: false
+		};
+	}
 
-  componentDidMount() {
-    this.setState({ loading: true }, () => {
-      const userListCount = 100;
-      getInitialUserListData(userListCount)
-        .then(res => {
-          // Slim down the user list data to only get relevant data that is needed for the app
-          const relevantUserDataList = getRelevantUserDataList(res.data.results);
+	componentDidMount() {
+		this.setState({ loading: true }, () => {
+			const userListCount = 100;
+			getInitialUserListData(userListCount)
+				.then(res => {
+					// Slim down the user list data to only get relevant data that is needed for the app
+					const relevantUserDataList = getRelevantUserDataList(res.data.results);
 
-          this.setState({
-            users: relevantUserDataList,
-            loading: false
-          });
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-    });
-  }
+					this.setState({
+						users: relevantUserDataList,
+						loading: false
+					});
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+		});
+	}
 
-  renderUserList(){
-    const { loading, users, filteredUsers } = this.state;
+	renderUserList(){
+		const { loading, users, filteredUsers } = this.state;
 
-    // If we have matches for filtered users display them otherwise
-    // display the initial list of all the users
-    const usersToDisplay = filteredUsers.length ? filteredUsers : users;
+		// If we have matches for filtered users display them otherwise
+		// display the initial list of all the users
+		const usersToDisplay = filteredUsers.length ? filteredUsers : users;
 
-    if (loading) {
-      return (<Spinner message="Loading..." />)
-    } else {
-      // Render user list when we have initial users back from request api
-      return (<UserList users={usersToDisplay} />)
-    }
-  }
+		if (loading) {
+			return (<Spinner message="Loading..." />)
+		} else {
+			// Render user list when we have initial users back from request api
+			return (<UserList users={usersToDisplay} />)
+		}
+	}
 
-  handleSearchChange(searchString) {
-    const filtererdResults = getFilteredResultsForUsers(this.state.users, searchString);
-    let filteredUserList =  [];
+	handleSearchChange(searchString) {
+		const filtererdResults = getFilteredResultsForUsers(this.state.users, searchString);
+		let filteredUserList =  [];
 
-    // There are filtered user results to display
-    if (filtererdResults < this.state.users) {
-      filteredUserList = filtererdResults;
-    }
+		// There are filtered user results to display
+		if (filtererdResults < this.state.users) {
+			filteredUserList = filtererdResults;
+		}
 
-    this.setState({
-      filteredUsers: filteredUserList
-    });
-  }
+		this.setState({
+			filteredUsers: filteredUserList
+		});
+	}
 
-  render() {
-    const { users, filteredUsers } = this.state;
-    const resultCount = filteredUsers.length ? filteredUsers.length : users.length;
-    return (
-      <div className="App">
-        <h1>User Directory</h1>
-        <UserSearch resultCount={resultCount} onChange={(e) => this.handleSearchChange(e)} />
-        {this.renderUserList()}
-      </div>
-    );
-  }
+	render() {
+		const { users, filteredUsers } = this.state;
+		const resultCount = filteredUsers.length ? filteredUsers.length : users.length;
+		return (
+			<div className="App">
+				<h1>User Directory</h1>
+				<UserSearch resultCount={resultCount} onChange={(e) => this.handleSearchChange(e)} />
+				{this.renderUserList()}
+			</div>
+		);
+	}
 }
 
 export default App;
