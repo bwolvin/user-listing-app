b2ae085904096d34f7dbe6cae32a68a1b888956d
Brandon Wolvin <wolvinb1@uoiitwolvinb1.local>
2018-05-19 00:25:20 -0400

    clean up

--- src/components/users/userListItem.js
+++ src/components/users/userListItem.js
@@ -4,15 +4,15 @@ import UserMeta from './userMeta';
 import '../../css/userListItem.css';
 
 class UserListItem extends Component {
-	render() {
-		const userDetails = this.props.user;
-		return (
-			<li className="user-list-item">
-				<UserImage userDetails={userDetails} />
-				<UserMeta userDetails={userDetails} />
-			</li>
-		);
-	}
+    render() {
+        const userDetails = this.props.user;
+        return (
+            <li className="user-list-item">
+                <UserImage userDetails={userDetails} />
+                <UserMeta userDetails={userDetails} />
+            </li>
+        );
+    }
 }
 
 export default UserListItem;
